073f321923f62b975faac8205d344d74386f318e
unknown <jaume.llopis1@gmail.com>
2019-12-07 02:12:29 +0100

    Renderer and First Click Events

--- sceneplayer.js
+++ sceneplayer.js
@@ -1,6 +1,4 @@
-
 /**
- * 
  * @param {type} id
  * @returns {ScenePlayer}
  */
@@ -37,12 +35,16 @@ function ScenePlayer( id ){
         /**
          * @type Audio[]
          */
-        'media':[]
+        'media':[],
+        /**
+         * @type Number
+         */
+        'framerate': 30
     };
     /**
-     * @type SceneLoader
+     * @type SceneDataBase
      */
-    var _loader = new SceneLoader();
+    var _DB = new SceneDataBase();
     /**
      * @param {Audio|String} media
      * @returns {ScenePlayer}
@@ -79,6 +81,7 @@ function ScenePlayer( id ){
                 _MANAGER.images[ source ] = this;
                 console.log( '[' + source + '] loaded (' + this.width + 'x' + this.height + ')' );
             };
+
             img.onerror = function(){
 
                 console.log( 'Error loading [' + source + ']' );
@@ -94,19 +97,22 @@ function ScenePlayer( id ){
      * @returns {ScenePlayer}
      */
     this.addSprite = function( setup ){
-
+        //console.log(setup);
         var sprite = new Sprite({
-            'image_id': typeof setup.image_id === 'number' ? setup.image_id : false,
+            'image': typeof setup.image === 'string' ? setup.image : false,
             'name': typeof setup.name === 'string' ? setup.name : 'New Sprite',
             'src': typeof setup.src === 'string' ? setup.src : '',
-            'rows': typeof setup.rows === 'number' ? setup.rows : 0,
-            'cols': typeof setup.cols === 'number' ? setup.cols : 0,
+            'rows': typeof setup.rows === 'number' ? setup.rows : 1,
+            'cols': typeof setup.cols === 'number' ? setup.cols : 1,
             'width': typeof setup.width === 'number' ? setup.width : 0,
             'height': typeof setup.height === 'number' ? setup.height : 0,
-            'time': typeof setup.time === 'number' ? setup.time : 100
+            'time': typeof setup.time === 'number' ? setup.time : 100,
+            'frames': Array.isArray( setup.frames ) ? setup.frames : [ 0 ],
+            'speed': setup.speed || 1.0
         });
-        
+        //console.log(sprite.validate());
         if( sprite.validate() ){
+            //console.log(sprite.validate());
             _MANAGER.sprites.push(sprite);
         }
         
@@ -125,10 +131,14 @@ function ScenePlayer( id ){
      */
     this.images = function(){ return _MANAGER.images; };
     /**
-     * @param {Number} media_id
+     * @param {Number|String} media_id
      * @returns {ScenePlayer}
      */
-    this.playSound = media_id => {
+    this.playSound = (media_id) => {
+
+        if( typeof media_id ==='undefined' || media_id === 'random' ){
+            media_id = Math.floor( Math.random() * _MANAGER.media.length );
+        }
 
         if( typeof media_id === 'number' && _MANAGER.media.length > media_id ){
 
@@ -152,70 +162,70 @@ function ScenePlayer( id ){
      * @param {Number} y
      * @returns {ScenePlayer}
      */
-    this.testSprite = function( sprite_id , x , y ){
+    this.testSprite = function( sprite_id , x , y , scale ){
         
-        
+       if( typeof sprite_id == 'number' ){
+           if( sprite_id >= 0 && _MANAGER.sprites.length > sprite_id ){
+               _MANAGER.sprites[ sprite_id ].move(x,y).scale( scale ).status( Sprite.Status.Active );
+           }
+       }
+       else if( typeof sprite_id === 'string' ){
+           _MANAGER.sprites.forEach( function(sprite){
+               if( sprite.name() === sprite_id ){
+                   sprite.move(x,y).scale( scale ).status( Sprite.Status.Active );
+               }
+           });
+       }
         
        return this; 
     };
     /**
-     * @param {Number} media_id
      * @returns {ScenePlayer}
      */
-    this.testSound = function( media_id ){
+    this.update = function( elapsed ){
+        
+        _MANAGER.sprites.forEach( function( sprite ){
 
-        document.addEventListener('DOMContentLoaded',function(e){
+            if ( sprite.area().right() > _MANAGER.canvas.width || sprite.area().left() < 0 ){
+                sprite.speed().X *= -1;
+            }
             
-            var btn = document.createElement('button');
+            if ( sprite.area().bottom() > _MANAGER.canvas.height || sprite.area().top() < 0 ){
+                sprite.speed().Y *= -1;
+            }
+
+            //logics here
+            sprite.update( elapsed );
             
-            btn.innerHTML = 'Play Sound';
-            
-            btn.addEventListener('click',function(e){
-
-                e.preventDefault();
-
-                _controller.playSound( typeof media_id === 'number' ?
-                        media_id :
-                        Math.floor(Math.random() * _controller.sounds().length));
-                
-                return true;
-            });
-            document.body.appendChild(btn);
-
         });
-
-        return this;
-    };
-    
-    
-    this.update = function(){
-        
         
         return this;
     };
-    
+    /**
+     * @returns {ScenePlayer}
+     */
     this.render = function(){
-            // Clear the canvas
-            _MANAGER.display.clearRect(0,0,_MANAGER.display.width,_MANAGER.display.height);
+        
+        //window.requestAnimationFrame( this.render );
+        // Clear the canvas
+        _MANAGER.display.clearRect(0,0,_MANAGER.canvas.width,_MANAGER.canvas.height);
+
         _MANAGER.sprites.forEach( function(sprite){
-            
-            if( sprite.validate() ){
+            if( _MANAGER.images[ sprite.image() ] instanceof Image ){
                 //display
                 _MANAGER.display.drawImage(
                     //bitmap de origen
-		    _MANAGER.images[ sprite.imageId() ],
+                    _MANAGER.images[ sprite.image() ],
                     //SX - columna desde la imagen de origen
-                    sprite.currentFrame.tileX(),
-		    //sprite.frameId() * sprite.width() / sprite.frameCount(),
+                    sprite.currentFrame.column() * sprite.tileWidth(),
                     //SY - fila desde la imagen de origen
-                    sprite.currentFrame.tileY(),
-		    //sprite.width() / sprite.frameCount(),
+                    sprite.currentFrame.row() * sprite.tileHeight(),
                     //SW,SH - dimension desde el area del bitmap de origen a recortar
-                    sprite.width(), sprite.height(),
+                    sprite.tileWidth(), sprite.tileHeight(),
                     //DX,DY - posición de destino sobre el display
-		    sprite.position().X, sprite.position().Y,
+                    sprite.X(), sprite.Y(),
                     //DW,DH - dimension sobre el display
-		    sprite.width(), sprite.height());
+                    sprite.width(), sprite.height());
             }
         });
         
@@ -226,6 +236,7 @@ function ScenePlayer( id ){
      * @returns {ScenePlayer}
      */
     this.resize = function(){
+
         _MANAGER.canvas.width = _MANAGER.container.offsetWidth;
 
         _MANAGER.canvas.height = _MANAGER.container.offsetHeight;
@@ -249,7 +260,50 @@ function ScenePlayer( id ){
      * @returns {Element}
      */
     this.getContainer = function(){ return document.getElementById(_MANAGER.id); };
-    
+    /**
+     * @param {Vector|Area} element
+     * @returns {Boolean}
+     */
+    this.inDisplay = function( element ){
+        
+        if( element instanceof Area ){
+            
+            return element.left() < _MANAGER.canvas.width
+                && element.right() > 0
+                && element.top() > 0
+                && element.bottom() < _MANAGER.canvas.height;
+        }
+        
+        if( element instanceof Vector ){
+            
+            return element.X >= 0 && element.X < _MANAGER.canvas.width
+                    && element.Y >= 0 && element.Y < _MANAGER.canvas.height;
+        }
+        
+        
+        return false;
+    };
+    /**
+     * @param {Event} e
+     * @returns {Boolean}
+     */
+    this.parseClickEvent = function( e ){
+        
+        for( var s = _MANAGER.sprites.length - 1 ; s > -1 ; s-- ){
+            
+            if( _MANAGER.sprites[ s ].click( e.offsetX , e.offsetY ) ){
+                
+                _controller.playSound( );
+                
+                console.log(_MANAGER.sprites[ s ].name() + ' clicked!');
+                
+                //quit from click loop
+                break;
+            }
+        }
+        
+        return true;
+    };
     /**
      * @returns {ScenePlayer}
      */
@@ -260,16 +314,22 @@ function ScenePlayer( id ){
             _MANAGER.container = document.getElementById(_MANAGER.id);
             
             _MANAGER.canvas = document.createElement('canvas');
-
+            
             _controller.resize();
+            
+            _MANAGER.canvas.addEventListener( 'click' , _controller.parseClickEvent );
 
             _MANAGER.display = _MANAGER.canvas.getContext('2d');
 
             _MANAGER.container.appendChild(_MANAGER.canvas);
 
-
-            //console.log(_MANAGER.sprites);
-            _controller.load();
+            _controller.load().render();
+            
+            window.setInterval( function(){
+                
+                _controller.update().render();
+                
+            }, 150 );
         });
         
         return this;
@@ -278,43 +338,35 @@ function ScenePlayer( id ){
     return this.init();
 }
 /**
- * @returns {SceneLoader}
+ * @returns {SceneDataBase}
  */
-function SceneLoader(){
-    /**
-     * @type Array
-     */
-    var _collection = [
-        //{'luna-char': {'src':'LunaChar48x96.png','cols':3,'rows':4}}
-    ];
-    /**
-     * @param {String} path
-     * @param {String} src
-     * @param {Number} cols
-     * @param {Number} rows
-     * @returns {SceneLoader}
-     */
-    this.add = function( path, src, cols, rows, width, height){
-        _collection.push({
-            'name':path,
-            'src':src,
-            'cols':cols,
-            'rows':rows,
-            'width':width,
-            'height':height
-        });
-        return this;
+function SceneDataBase( path ){
+
+    var _DB = {
+        'images':[],
+        'scenes':[]
     };
     /**
      * @returns {Array}
      */
-    this.dictionary = function(){ return _collection; };
+    this.db = function(){ return _DB; };
     
     return this;
 }
 
 function Scene(){
+
+    var _SCENE = {
+        'name':'',
+        'sprites':[],
+        'events':[]
+    };
     
+    this.addScene = function(){
+        
+    };
+
+    return this;
 }
 
 function EventPage(){
@@ -336,74 +388,231 @@ function Vector( x , y ){
     
     this.X = typeof x === 'number' ? x : 0;
     
-    this.Y = typeof y === 'number' ? y : 0;
+    this.Y = typeof y === 'number' ? y : this.X;
+    /**
+     * @param {Number} x
+     * @param {Number} y
+     * @returns {Vector}
+     */
+    this.set = ( x , y ) => {
+        this.X = x;
+        this.Y = typeof y === 'number' ? y : this.X;
+        return this;
+    };
+    /**
+     * @param {Number} x
+     * @param {Number} y
+     * @returns {Vector}
+     */
+    this.add = ( x , y ) => {
+        this.X += x;
+        this.Y += typeof y === 'number' ? y : x;
+        return this;
+    };
+    /**
+     * @param {Number} x
+     * @param {Number} y
+     * @returns {Vector}
+     */
+    this.mult = ( x , y ) => {
+        this.X *= x;
+        this.Y *= typeof y === 'number' ? y : x;
+        return this;
+    };
 }
 /**
  * 
  * @returns {Vector}
  */
 Vector.Zero = (function(){ return new Vector(0,0); })();
-Object.freeze(Vector.Zero);
+/**
+ * @type Vector
+ */
+Vector.One = (function(){ return new Vector(1,1); })();
+//Object.freeze(Vector.Zero);
 /**
  * 
- * @param {type} setup
+ * @param {Number} x
+ * @param {Number} y
+ * @param {Number} width
+ * @param {Number} height
+ * @returns {Area}
+ */
+function Area( x , y , width,  height ){
+    
+    this.X = x;
+    this.Y = y;
+    this.width = width;
+    this.height = height;
+    
+    this.left = () => this.X;
+    this.right = () => this.X + this.width;
+    this.top = () => this.Y;
+    this.bottom = () => this.Y + this.height;
+    /**
+     * 
+     * @param {Vector|Area} element
+     * @returns {Boolean}
+     */
+    this.intersect = ( element ) => {
+        
+        if( element instanceof Vector ){
+            
+            return ( element.X >= this.left() && element.X <= this.right() )
+                && ( element.Y >= this.top() && element.Y <= this.bottom() );
+        }
+        
+        return false;
+    };
+    
+    return this;
+}
+
+/**
+ * 
+ * @param {Object} setup
  * @returns {Sprite}
  */
 function Sprite( setup ){
-    
+
     var _sprite = {
-        'image': typeof setup.image_id === 'number' ? setup.image_id : false,
-        'src': typeof setup.src === 'string' ? setup.src : '',
+        'image': typeof setup.image === 'string' ? setup.image : false,
         'rows': typeof setup.rows === 'number' ? setup.rows : 1,
         'cols': typeof setup.cols === 'number' ? setup.cols : 1,
-        'width': typeof setup.width === 'number' ? setup.width : 0,
-        'height': typeof setup.height === 'number' ? setup.height : 0,
+        'imageWidth': typeof setup.width === 'number' ? setup.width : 0,
+        'imageHeight': typeof setup.height === 'number' ? setup.height : 0,
+        'scale': 1.0,
         'name': typeof setup.name === 'string' ? setup.name : 'New Sprite',
+        'speed': typeof setup.speed === 'number' ? new Vector( setup.speed ) : Vector.One(),
         //sprite runtime properties
         'ticks': typeof setup.time === 'number' ? setup.time : 100,
         //frame collection (columns and rows)
-        'frames': [],
+        'frames': Array.isArray( setup.frames ) ? setup.frames : [0],
         //frame index
         'frame_id': 0,
         'position': Vector.Zero,
+        'status': Sprite.Status.Inactive
+    };
+    /**
+     * @param {Number} x 
+     * @returns {Number}
+     */
+    this.X = function( x ){
+    
+        if( typeof x === 'number'){
+
+            _sprite.position.X = x;
+        }
+        
+        return _sprite.position.X;
+    };
+    /**
+     * @param {Number} y 
+     * @returns {Number}
+     */
+    this.Y = function( y ){
+    
+        if( typeof y === 'number'){
+
+            _sprite.position.Y = y;
+        }
+
+        return _sprite.position.Y;
+    };
+    /**
+     * @param {Number} scale
+     * @returns {Sprite}
+     */
+    this.scale = ( scale ) =>{
+        
+        if( typeof scale === 'number' ){
+            _sprite.scale = scale;
+        }
+
+        return this;
+    };
+    /**
+     * @returns {Vector}
+     */
+    this.speed = ( speed ) => {
+        
+        if( typeof speed === 'number' ){
+            _sprite.speed = speed;
+        }
+        
+        return _sprite.speed;
     };
     /**
      * @returns {Boolean}
      */
     this.validate = function(){
+
+        if( _sprite.image !== false && _sprite.image.length && _sprite.frames.length > 0 ){
+            
+            if( _sprite.status === Sprite.Status.Invalid ){
+                
+                _sprite.status = Sprite.Status.Hidden;
+            }
+            
+            return true;
+        }
         
-        return _sprite.src.length && _sprite.name.length && _sprite.image !== false;
+        return false;
+    };
+    /**
+     * @returns {Boolean}
+     */
+    this.valid = function(){ return _sprite.status > Sprite.Status.Invalid; };
+    /**
+     * @param {Number} newStatus 
+     * @returns {Number}
+     */
+    this.status = function( newStatus ){
+        
+        if( _sprite.status > Sprite.Status.Invalid ){
+            if( typeof newStatus === 'number' && _sprite.status !== newStatus ){
+                _sprite.status = newStatus;
+            }
+        }
+        
+        return _sprite.status;
     };
     /**
      * @returns {Number}
      */
-    this.width = function(){
-        return _sprite.width;
-    };
+    this.width = function(){ return Math.floor( _sprite.imageWidth / _sprite.cols ) * _sprite.scale; };
     /**
      * @returns {Number}
      */
-    this.height = function(){
-        return _sprite.height;
-    };
+    this.height = function(){ return Math.floor( _sprite.imageHeight / _sprite.rows ) * _sprite.scale; };
+    /**
+     * @returns {Number} 
+     */
+    this.tileWidth = () => parseInt( _sprite.imageWidth / _sprite.cols );
+    /**
+     * @returns {Number} 
+     */
+    this.tileHeight = () => parseInt( _sprite.imageHeight / _sprite.rows );
     /**
      * @returns {Number}
      */
-    this.cols = function(){ return _sprite.cols; };
+    this.columns = () => _sprite.cols;
     /**
      * @returns {Number}
      */
-    this.rows = function(){ return _sprite.rows; };
+    this.rows = () => _sprite.rows;
     /**
      * @returns {Number}
      */
-    this.index = function(){
-        return 0;
-    };
+    this.index = function(){ return 0; };
+    /**
+     * @returns {String}
+     */
+    this.name = function(){ return _sprite.name; };
     /**
      * @returns {Number}
      */
-    this.imageId = function(){ return _sprite.image; };
+    this.image = function(){ return _sprite.image; };
     /**
      * @returns {Number}
      */
@@ -413,18 +622,86 @@ function Sprite( setup ){
      */
     this.frameCount = function(){ return _sprite.frames.length; };
     /**
+     * @param {Number} x 
+     * @param {Number} y 
+     * @returns {Sprite}
+     */
+    this.move = ( x , y ) => {
+
+        if( typeof x === 'number' ){
+            _sprite.position.X += _sprite.speed.X;
+        }
+        
+        if( typeof y === 'number' ){
+            _sprite.position.Y += _sprite.speed.Y;
+        }
+        
+        return this;
+    };
+    /**
      * @returns {Vector}
      */
-    this.position = function(){ return _sprite.position; };
-    
+    this.position = function( x , y ){
+        
+        return _sprite.position;
+    };
+    /**
+     * @returns {Area}
+     */
+    this.area = () => new Area( _sprite.position.X , _sprite.position.Y , this.width() , this.height() );
+    /**
+     * @param {Number} x
+     * @param {Number} y
+     * @returns {Boolean}
+     */
+    this.click = function( x ,y ){
+        
+        if( this.area().intersect(new Vector(x,y))){
+
+            return true;
+        }
+        
+        return false;
+    };
+    /**
+     * @param {Number} elapsed
+     * @returns {Sprite}
+     */
+    this.update = function( elapsed ){
+        
+        if( _sprite.frame_id < _sprite.frames.length - 1 ){
+            _sprite.frame_id++;
+        }
+        else{
+            _sprite.frame_id = 0;
+        }
+        
+        _sprite.position.X++;
+        _sprite.position.Y++;
+
+        return this;
+    };
+    /**
+     * @type Sprite.currentFrame
+     */
     this.currentFrame = {
-        'id': function(){ return _sprite.frame_id; },
-        'tileX': function(){ return _sprite.frame_id * _sprite.width / _sprite.frames.length; },	    
-        'tileY': function(){ return _sprite.width / _sprite.frames.length; }
+        'test': () => Math.floor( _sprite.frames[ _sprite.frame_id ] / _sprite.cols ),
+        'id': ()=> _sprite.frames[ _sprite.frame_id ],
+        'column': () => parseInt( _sprite.frames[ _sprite.frame_id ] % _sprite.cols ),
+        'row': () => parseInt( _sprite.frames[ _sprite.frame_id ] / _sprite.cols )
     };
     
     return this;
 }
+/**
+ * @type Object
+ */
+Sprite.Status = {
+    'Invalid': 0,
+    'Hidden': 1,
+    'Active': 2,
+    'Removed':3
+};
 /**
  * @param {Object} params
  * @returns {SoundBank}
